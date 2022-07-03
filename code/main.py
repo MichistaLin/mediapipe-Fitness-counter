db082be328b17e9667715b965c777aea79754411
MichistaLin <3166306093@qq.com>
2022-07-04 00:49:21 +0800

    update

--- code/main.py
+++ code/main.py
@@ -1,6 +1,21 @@
 import videoprocess as vp
 import trainingsetprocess as tp
+import videocapture as vc
 
 if __name__ == '__main__':
     tp.trainset_process()
-    vp.video_process()
+    while True:
+        menu = int(input("请输入检测模式（数字）：1. 从本地导入视频检测\t2. 调用摄像头检测\n"))
+        if menu == 1:
+            vp.video_process()
+            break
+        elif menu == 2:
+            vc.process()
+            break
+        else:
+            print("输入错误，请重新输入！")
+            continue
+
+
+
+
