b81e402cc6c6288e5afa8c246947577187f8afcd
MichistaLin <3166306093@qq.com>
2022-07-04 19:08:47 +0800

    update2

--- code/main.py
+++ code/main.py
@@ -5,12 +5,15 @@ import videocapture as vc
 if __name__ == '__main__':
     tp.trainset_process()
     while True:
-        menu = int(input("请输入检测模式（数字）：1. 从本地导入视频检测\t2. 调用摄像头检测\n"))
+        menu = int(input("请输入检测模式（数字）：1. 从本地导入视频检测\t2. 调用摄像头检测\t3. 退出\n"))
         if menu == 1:
             vp.video_process()
-            break
+            continue
         elif menu == 2:
+            print("\n按键q或esc退出摄像头采集")
             vc.process()
+            continue
+        elif menu == 3:
             break
         else:
             print("输入错误，请重新输入！")
